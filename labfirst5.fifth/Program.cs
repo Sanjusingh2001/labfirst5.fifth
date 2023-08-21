895128bdeb433ceac43bd30dcba8bb77b678923c
hp <hp@DESKTOP-75BFR6A>
2023-08-21 22:56:26 +0530

    Add project files.

---
+++ labfirst5.fifth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labfirst5.fifth
+{
+    class fifth
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = { 45, 67, 32, 78, 89 };
+            Console.WriteLine("The original array : ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(Arr[i] + " ");
+            }
+            int k = 0;
+            int[] reArr = new int[5];
+            for (int i = 4; i >= 0; i--)
+            {
+                reArr[k++] = Arr[i];
+            }
+            Console.WriteLine("\nAfter reverse The  array : ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(reArr[i] + " ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
